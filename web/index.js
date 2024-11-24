817fdcee9559f497bf5fcfdffbbec1f24990a5f5
Billy Moore <billyedmoore@yahoo.com>
2024-11-24 11:54:15 +0000

    Changed footer from fixed to more traditional, removed dark and light modes (dark only).

--- web/index.js
+++ web/index.js
@@ -44,15 +44,10 @@ function fetchStateFromOpenSky(config) {
 			var t = document.getElementById("image-target");
 			t.innerHTML = "" // Clear existing_children
 
-			var zero_colour = [10, 10, 10, 255]
-			var one_colour = [250, 250, 250, 255]
+			var one_colour = [10, 10, 10, 255]
+			var zero_colour = [250, 250, 250, 255]
 
-			if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-				temp = zero_colour
-				zero_colour = one_colour
-				one_colour = temp
-			}
-			img_multiplier = Math.floor((window.innerWidth / arr[0].length) * 0.8)
+			img_multiplier = 30
 			create_canvas_from_pixels(t, arr, img_multiplier, one_colour = one_colour, zero_colour = zero_colour)
 			return [return_val[0], arr]
 		}).then((return_val) => console.log(return_val[0]))
